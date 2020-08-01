be87e80fa827af0b660299352684188fcd95104a
YAMAMI Taro <mitarothanken@gmail.com>
2020-08-01 13:58:13 +0900

    WPF ビジュアル ツリーにコンテンツを追加するための UserControl を作成する
    https://docs.microsoft.com/ja-jp/windows/apps/desktop/modernize/using-the-visual-layer-with-wpf#create-a-usercontrol-to-add-your-content-to-the-wpf-visual-tree

---
+++ HelloCompositionCore/CompositionHostControl.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.UI.Composition;
+
+namespace HelloCompositionCore
+{
+    /// <summary>
+    /// CompositionHostControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class CompositionHostControl : UserControl
+    {
+        CompositionHost compositionHost;
+        Compositor compositor;
+        Windows.UI.Composition.ContainerVisual containerVisual;
+        DpiScale currentDpi;
+        
+        public CompositionHostControl()
+        {
+            InitializeComponent();
+            Loaded += CompositionHostControl_Loaded;
+            compositionHost.DpiChanged += CompositionHost_DpiChanged;
+        }
+
+        private void CompositionHost_DpiChanged(object sender, DpiChangedEventArgs e)
+        {
+            currentDpi = e.NewDpi;
+            Vector3 newScale = new Vector3((float)e.NewDpi.DpiScaleX, (float)e.NewDpi.DpiScaleY, 1);
+
+            foreach (SpriteVisual child in containerVisual.Children)
+            {
+                child.Scale = newScale;
+                var newOffsetX = child.Offset.X * ((float)e.NewDpi.DpiScaleX / (float)e.OldDpi.DpiScaleX);
+                var newOffsetY = child.Offset.Y * ((float)e.NewDpi.DpiScaleY / (float)e.OldDpi.DpiScaleY);
+                child.Offset = new Vector3(newOffsetX, newOffsetY, 1);
+
+                // Adjust animations for DPI change.
+                AnimateSquare(child, 0);
+            }
+        }
+
+        private void CompositionHostControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // If the user changes the DPI scale setting for the screen the app is on,
+            // the CompositionHostControl is reloaded. Don't redo this set up if it's
+            // already been done.
+            if (compositionHost is null)
+            {
+                currentDpi = VisualTreeHelper.GetDpi(this);
+
+                compositionHost =
+                    new CompositionHost(CompositionHostElement.ActualHeight, CompositionHostElement.ActualWidth);
+                CompositionHostElement.Child = compositionHost;
+                compositor = compositionHost.Compositor;
+                containerVisual = compositor.CreateContainerVisual();
+                compositionHost.Child = containerVisual;
+            }
+        }
+        // Add
+        // using System.Numerics;
+
+        public void AddElement(float size, float offsetX, float offsetY)
+        {
+            var visual = compositor.CreateSpriteVisual();
+            visual.Size = new Vector2(size, size);
+            visual.Scale = new Vector3((float)currentDpi.DpiScaleX, (float)currentDpi.DpiScaleY, 1);
+            visual.Brush = compositor.CreateColorBrush(GetRandomColor());
+            visual.Offset = new Vector3(offsetX * (float)currentDpi.DpiScaleX, offsetY * (float)currentDpi.DpiScaleY, 0);
+
+            containerVisual.Children.InsertAtTop(visual);
+
+            AnimateSquare(visual, 3);
+        }
+
+        private void AnimateSquare(SpriteVisual visual, int delay)
+        {
+            float offsetX = (float)(visual.Offset.X); // Already adjusted for DPI.
+
+            // Adjust values for DPI scale, then find the Y offset that aligns the bottom of the square
+            // with the bottom of the host container. This is the value to animate to.
+            var hostHeightAdj = CompositionHostElement.ActualHeight * currentDpi.DpiScaleY;
+            var squareSizeAdj = visual.Size.Y * currentDpi.DpiScaleY;
+            float bottom = (float)(hostHeightAdj - squareSizeAdj);
+
+            // Create the animation only if it's needed.
+            if (visual.Offset.Y != bottom)
+            {
+                Vector3KeyFrameAnimation animation = compositor.CreateVector3KeyFrameAnimation();
+                animation.InsertKeyFrame(1f, new Vector3(offsetX, bottom, 0f));
+                animation.Duration = TimeSpan.FromSeconds(2);
+                animation.DelayTime = TimeSpan.FromSeconds(delay);
+                visual.StartAnimation("Offset", animation);
+            }
+        }
+
+        private Windows.UI.Color GetRandomColor()
+        {
+            Random random = new Random();
+            byte r = (byte)random.Next(0, 255);
+            byte g = (byte)random.Next(0, 255);
+            byte b = (byte)random.Next(0, 255);
+            return Windows.UI.Color.FromArgb(255, r, g, b);
+        }
+    }
+}
