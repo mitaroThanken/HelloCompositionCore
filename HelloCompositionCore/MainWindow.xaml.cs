2bb18087e439ba84dc4d8a706f6d0c8c41f81bde
YAMAMI Taro <mitarothanken@gmail.com>
2020-08-01 14:14:34 +0900

    XAML ページにユーザー コントロールを追加する
    https://docs.microsoft.com/ja-jp/windows/apps/desktop/modernize/using-the-visual-layer-with-wpf#add-the-user-control-to-your-xaml-page

--- HelloCompositionCore/MainWindow.xaml.cs
+++ HelloCompositionCore/MainWindow.xaml.cs
@@ -24,5 +24,14 @@ namespace HelloCompositionCore
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            float size = random.Next(50, 150);
+            float offsetX = random.Next(0, (int)(CompositionHostControl1.ActualWidth - size));
+            float offsetY = random.Next(0, (int)(CompositionHostControl1.ActualHeight / 2 - size));
+            CompositionHostControl1.AddElement(size, offsetX, offsetY);
+        }
     }
 }
